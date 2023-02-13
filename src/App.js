71a4e2029a2d47a8cef852ab26dc3f026c54e7da
Ian Ruvuto <tallerian5@gmail.com>
2023-02-13 19:42:14 +0100

    react js portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import Profile_text from './Profile_text';
 
 function App() {
+
+  const profile_txt = document.getElementById('myprofile-text')
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <body>
+    <div class="container">
+      {/*=============== myprofile_container =============== */}
+            <div className="myprofile_container">
+                <div className="myprofile"></div>
+                <div className="ml10">
+                  <Profile_text p_txt = {profile_txt}/>
+                </div>
+            </div>
     </div>
+
+    <div className="icon-links">
+                <a href="https://www.instagram.com/ian_kampwanyi/" target="_blank">
+                    <div class = "ico_circle">
+                            <span class = "icon"><i class="ri-instagram-line fa-lg" id = "insta_ico"></i></span>
+                    </div>
+                </a>
+                <a href="https://github.com/Tallerian" target="_blank">
+                    <div class = "ico_circle">
+                            <span class = "icon"><i class="ri-github-line" id="github_ico"></i></span>
+                    </div>
+                </a>
+                <a href="https://www.linkedin.com/in/ian-kampwanyi-ruvuto-63a531177/" target="_blank">
+                    <div class = "ico_circle">
+                            <span class = "icon"><i class="ri-linkedin-box-line" id="linked_ico"></i></span>
+                    </div>
+                </a>
+            </div>
+    </body>
   );
 }
 
---
+++ src/Profile_text.js
@@ -0,0 +1,31 @@
+import React, {Component, useState} from 'react';
+import './styles/Profile_text.css';
+import Typewriter from 'typewriter-effect';
+
+function Profile_text(){
+
+    const phrases = ['Hello my name is Ian Ruvuto', 'I am developer', 'Welcome to my CV-look alike portfolio']
+
+    return(
+        <div className="ml10">
+        <span className="text-wrapper">
+            <div className="myprofile-text" id = "myprofile-text"></div>
+            <div className="myprofile-text">
+            <Typewriter 
+                    options={
+                        {
+                            autoStart: true,
+                            loop: true,
+                            delay: 30,
+                            strings: phrases,
+                            pauseFor: 1000,
+
+                        }   
+                    }
+                />
+            </div>    
+        </span>
+        </div>
+    )
+}
+export default Profile_text
