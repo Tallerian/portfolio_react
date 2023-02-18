07015764db0998f15db10bbcdd4c4fabdb5704c2
Ian Ruvuto <tallerian5@gmail.com>
2023-02-18 21:44:23 +0100

    portfilio cv and linkss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './styles/App.css';
+
 import Profile_text from './Profile_text';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
 
@@ -17,25 +20,40 @@ function App() {
                   <Profile_text p_txt = {profile_txt}/>
                 </div>
             </div>
+
+    
+    {/*=============== Linkssss =============== */}
+    <div className="icon-links">
+        <a href="https://www.instagram.com/ian_kampwanyi/" target="_blank">
+            <div class = "ico_circle">
+                    <span class = "icon"> <FontAwesomeIcon icon = {faInstagram}/> </span>
+            </div>
+        </a>
+        <a href="https://github.com/Tallerian" target="_blank">
+            <div class = "ico_circle">
+                    <span class = "icon"><FontAwesomeIcon icon = {faGithub}/></span>
+            </div>
+        </a>
+        <a href="https://www.linkedin.com/in/ian-kampwanyi-ruvuto-63a531177/" target="_blank">
+            <div class = "ico_circle">
+                    <span class = "icon"><FontAwesomeIcon icon = {faLinkedin}/></span>
+            </div>
+        </a>
     </div>
 
-    <div className="icon-links">
-                <a href="https://www.instagram.com/ian_kampwanyi/" target="_blank">
-                    <div class = "ico_circle">
-                            <span class = "icon"><i class="ri-instagram-line fa-lg" id = "insta_ico"></i></span>
-                    </div>
-                </a>
-                <a href="https://github.com/Tallerian" target="_blank">
-                    <div class = "ico_circle">
-                            <span class = "icon"><i class="ri-github-line" id="github_ico"></i></span>
-                    </div>
-                </a>
-                <a href="https://www.linkedin.com/in/ian-kampwanyi-ruvuto-63a531177/" target="_blank">
-                    <div class = "ico_circle">
-                            <span class = "icon"><i class="ri-linkedin-box-line" id="linked_ico"></i></span>
-                    </div>
-                </a>
-            </div>
+    {/*=============== cv_container =============== */}
+    <div class="cv_container">
+                
+        <a href="" class="button_cv">
+            Download CV
+        </a>  
+
+    </div>
+
+
+    {/*=============== projects display =============== */}
+
+    </div>
     </body>
   );
 }
